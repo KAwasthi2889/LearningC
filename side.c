be780ed0341a9fd724cae491a0dfe17bf6058418
Kaustubh <kaustubh_awasthi@proton.me>
2023-12-08 20:22:08 +0530

    Adding else-if on side.c

--- side.c
+++ side.c
@@ -5,16 +5,12 @@ int main()
     printf("Write any 3 sides of triangle:\n");
     scanf("%d%d%d",&s1,&s2,&s3);
     if(s1+s2>s3&&s2+s3>s1&&s1+s3>s2)
-    {
-        if(s1==s2||s2==s3||s1==s3)
-            {   
-                if(s1==s2&&s2==s3&&s1==s3)
-                    printf("The sum of all the 3 sides of triangle is equal, hence they can form an equilateral triangle.\n");
-                else
-                    printf("The sum of 2 sides of triangle is greater than the 3rd side and its 2 sides are equal, hence they can form an isosceles triangle.\n");
-            }
+    {   if(s1==s2&&s2==s3&&s1==s3)
+            printf("The sum of all the 3 sides of triangle is equal, hence they can form an equilateral triangle.\n");
+        else if(s1==s2||s2==s3||s1==s3)
+            printf("The sum of 2 sides of triangle is greater than the 3rd side and its 2 sides are equal, hence they can form an isosceles triangle.\n");
         else
-        printf("The sum of 2 sides of triangle is greater than the 3rd side, hence they can form a triangle.\n");
+            printf("The sum of 2 sides of triangle is greater than the 3rd side, hence they can form a triangle.\n");
     }
     else
     printf("The sum of 2 sides of triangle is less than the 3rd side, hence they cannot form a triangle.\n");
