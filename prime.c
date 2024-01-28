529f19bc6a1116e71991d4b899f026672b4ca0f1
Kaustubh <kaustubh_awasthi@proton.me>
2024-01-28 13:14:01 +0530

    This is Shortened

--- prime.c
+++ prime.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
-int main()
+
+int main() 
 {
     int num;
     printf("Enter any number: ");
-    scanf("%d",&num);
-    if(num<=0)
-        printf("Please enter a positive number\n");
-    if(num==1)
-        printf("1 is neither prime nor a composite number\n");
-    for (int i=2; i<=num; i++)
+    scanf("%d", &num);
+    if (num <= 0) 
     {
-        if(i==num)
-        {   printf("%d is a prime number\n",num);
-            break;
-        }
-        if(num%i==0)
-        {   printf("%d is a composite number\n",num);
-            break;
-        }
-
+        printf("Please enter a positive number\n");
     }
+    if (num == 1) 
+    {
+        printf("1 is neither prime nor a composite number\n");
+    }
+    for (int i = 2; i < num; i++) 
+    {
+        if (num % i == 0) 
+        {
+            printf("%d is a composite number\n", num);
+            return 0;
+        }
+    }
+    printf("%d is a prime number\n", num);
     return 0;
 }
